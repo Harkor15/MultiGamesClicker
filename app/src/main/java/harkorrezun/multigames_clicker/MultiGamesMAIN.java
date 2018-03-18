360c3ca542915f46d2f442cc0d0f8bf4421dfdc5
Rezun Official <rezunofficial@gmail.com>
2018-03-18 22:38:00 +0100

    Initial commit

---
+++ app/src/main/java/harkorrezun/multigames_clicker/MultiGamesMAIN.java
@@ -0,0 +1,13 @@
+package harkorrezun.multigames_clicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiGamesMAIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_games_main);
+    }
+}

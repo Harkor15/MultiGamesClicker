fdeda03d975e35984fa6be0cb9a23cf1553eb473
Harkor <krzbigos@gmail.com>
2018-03-27 20:48:51 +0200

    First clicker layout done.

--- app/src/main/java/harkorrezun/multigames_clicker/F1_clicker.java
+++ app/src/main/java/harkorrezun/multigames_clicker/F1_clicker.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by Harkor on 2018-03-26.
@@ -13,10 +14,18 @@ import android.view.ViewGroup;
 
 public class F1_clicker extends Fragment {
 
+    int amount=0;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.f1_clicker,container,false);
         return view;
+    }
+
+
+    public void pianoClick(View view,int button){
+        Toast.makeText(getActivity(),"Butt: "+button,Toast.LENGTH_SHORT).show();
     }
 }
